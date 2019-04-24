5467ce84c8e76de545c9901dee7b065c7f2f2c1d
Yurij2015 <nabor_2008@i.ua>
2019-04-24 19:59:39 +0300

    add remember me2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,9 +11,17 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Возвращает текущего вошедшего пользователя (если есть).
+  # Возвращает пользователя, соответствующего remember-токену в куки.
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   # Возвращает true, если пользователь вошел, иначе false.
@@ -21,10 +29,17 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  # Забывает постоянную сессии.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   # Осуществляет выход текущего пользователя.
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
-
 end
